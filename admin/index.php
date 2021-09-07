5d5680cbb5d08ad8f748505298e835d5cabcc87e
GrazyRocha <graziela.rocha2@etec.sp.gov.br>
2021-09-07 20:47:14 -0300

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,89 @@
+<?php session_start()?>
+
+<!DOCTYPE html>
+<!-- saved from url=(0063)https://getbootstrap.com.br/docs/4.1/examples/starter-template/ -->
+<html lang="pt-br"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon/novo.ico">
+
+    <title>Achei Você.com</title>
+
+    <!-- Principal CSS do Bootstrap -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <!-- Estilos customizados para esse template -->
+    <link href="../css/starter-template.css" rel="stylesheet">
+    <!-- fonte icones -->
+
+    <script src="../js/fontawesome.js"></script>
+  </head>
+
+  <body class="bg-dark" >
+  
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="https://getbootstrap.com.br/docs/4.1/examples/starter-template/#">
+      Admin</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" 
+      aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/starter-template/#">
+            Home <span class="sr-only">(atual)</span></a>
+          </li>
+         
+          <li class="nav-item">
+            <a class="nav-link" href="https://getbootstrap.com.br/docs/4.1/examples/starter-template/#">
+            Link</a>
+          </li>
+         
+          <li class="nav-item">
+            <a class="nav-link disabled" href="https://getbootstrap.com.br/docs/4.1/examples/starter-template/#">
+            Desativado</a>
+          </li>
+          
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" 
+            aria-haspopup="true" aria-expanded="false">
+            <?php echo $_SESSION['nome'];?> </a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="https://getbootstrap.com.br/docs/4.1/examples/starter-template/#">
+              Editar perfil</a>
+              <a class="dropdown-item" href="https://getbootstrap.com.br/docs/4.1/examples/starter-template/#">
+              Alterar senha</a>
+          </div>
+          </li>
+        </ul>
+
+        <div class="my-2 my-lg-0">
+          <a href="../login.php" class="btn btn-outline-light my-2 my-sm-0" >
+            <i class="fas fa-sign-out-alt"></i>
+            Sair</a>
+        </div>
+      </div>
+    </nav>
+
+    <main role="main" class="container bg-dark border shadow rounded">
+
+    <div class="starter-template">
+        <img src="../imagens/procura.png">
+        <h1 class="text-warning"><i class="fas fa-search"></i>  Achei você.com</h1>
+        <p class="lead text-light font-weight-bold">Use este documento como uma maneira de iniciar um novo projeto, rapidamente.<br>Tudo oquê você começa é com este texto e um documento HTML (quase vazio).</p>
+      </div>
+
+    </main><!-- /.container -->
+
+    <!-- Principal JavaScript do Bootstrap
+    ================================================== -->
+    <!-- Foi colocado no final para a página carregar mais rápido -->
+    <script src="../js/jquery-3.3.1.slim.min.js"></script>
+    
+    <script src="../js/bootstrap.js"></script>
+  
+
+</body></html>
